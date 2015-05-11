a082327fff022cb261dc060cac4f44edb8bf4560
Brendan Rius <brendan.rius@gmail.com>
2015-05-11 13:42:16 +0200

    Tests VS

---
+++ raytracer/raytracer/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace raytracer
+{
+    public class Class1
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ raytracer/tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            raytracer.Class1 c = new raytracer.Class1();
+            Assert.AreEqual<int>(5, c.add(2, 3));
+        }
+    }
+}
