9ab2d3dd01b8fc0504a1f1809d15acf19fd6fda1
Louis Person <lait.kureuil@gmail.com>
2015-06-07 23:24:20 +0200

    yolosawj (bis)

--- raytracer/librt3/core/reflection/FresnelBlend.cs
+++ raytracer/librt3/core/reflection/FresnelBlend.cs
@@ -24,9 +24,22 @@ namespace librt3.core.reflection
             _rs = s;
         }
 
+        public SampledSpectrum SchlickFresnel(float costheta)
+        {
+            return _rs + (new SampledSpectrum(1) - _rs)*(float)Math.Pow(1 - costheta, 5);
+        }
+
         public override SampledSpectrum F(Vector3 incoming, Vector3 leaving)
         {
-            throw new NotImplementedException();
+            var diffuse = _rd*(float)(28f/(23*Math.PI))*(new SampledSpectrum(1) - _rs)*
+                          (1f - (float) Math.Pow(1 - 0.5*AbsCosTheta(ref incoming), 5))*
+                          (1f - (float) Math.Pow(1 - 0.5*AbsCosTheta(ref leaving), 5));
+            var half = (incoming + leaving).Normalized();
+            var specular = new SampledSpectrum(_distribution.D(ref half)) /
+                           (new SampledSpectrum(4 * AbsDot(ref incoming, ref leaving) *
+                            Math.Max(AbsCosTheta(ref incoming), AbsCosTheta(ref leaving))) *
+                            SchlickFresnel(Vector3.Dot(incoming, half)));
+            return diffuse + specular;
         }
     }
 }
