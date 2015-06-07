9a6546c9539e39a7daf4431c23365bca4dd8399a
Louis Person <lait.kureuil@gmail.com>
2015-06-07 20:46:52 +0200

    SampledSpectrum OrenNayar

--- raytracer/librt3/core/reflection/OrenNayar.cs
+++ raytracer/librt3/core/reflection/OrenNayar.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using OpenTK;
 using raytracer.core;
 using raytracer.core.mathematics;
@@ -25,7 +26,27 @@ namespace librt3.core.reflection
             var sinthetai = SinTheta(ref incoming);
             var sinthetao = SinTheta(ref leaving);
             var maxcos = 0f;
-            return SampledSpectrum.Black();
+            if (sinthetai > 1e-4 && sinthetao > 1e-4)
+            {
+                var sinphii = SinPhi(ref incoming);
+                var cosphii = CosPhi(ref incoming);
+                var sinphio = SinPhi(ref leaving);
+                var cosphio = CosPhi(ref leaving);
+                var dcos = cosphio*cosphii + sinphii*sinphio;
+                maxcos = Math.Max(0f, dcos);
+            }
+            float sinalpha, tanbeta;
+            if (AbsCosTheta(ref incoming) > AbsCosTheta(ref leaving))
+            {
+                sinalpha = sinthetao;
+                tanbeta = sinthetai/AbsCosTheta(ref incoming);
+            }
+            else
+            {
+                sinalpha = sinthetai;
+                tanbeta = sinthetao/AbsCosTheta(ref leaving);   
+            }
+            return _spectrum * (float)(1 / Math.PI) * (_a + _b * maxcos * sinalpha * tanbeta);
         }
     }
 }
